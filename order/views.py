c9ff467e55c39a294f7b19e4202d14e025bdbdf4
lhjwork <jin.come.up.business@gmail.com>
2022-09-15 22:41:59 +0900

    user 화면 구현 및 데이터 활용

--- order/views.py
+++ order/views.py
@@ -51,8 +51,17 @@ def order(request):
           address = request.POST['address']
           shop = request.POST['shop']
           order_date  = timezone.now()
-          
+          food_list = request.POST.getlist('menu')
           shop_item = Shop.objects.get(pk=int(shop))
           
           shop_item.order_set.create(address = address, order_date = order_date, shop=int(shop))
+          
+          order_item = Order.objects.get(pk = shop_item.order_set.latest('id').id)
+          
+          for food in food_list:
+               order_item.orderfood_set.create(food_name = food)
+          
           return HttpResponse(status=200)
+     elif request.method == 'GET':
+          order_list = Order.objects.all()
+          return render(request,'order/order_list.html',{'order_list':order_list})
