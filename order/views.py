cf2d2bd12971ec4841d1f34603fe9eef6530e372
lhjwork <jin.come.up.business@gmail.com>
2022-09-15 19:54:45 +0900

    order > shop 부분까지 진행

--- order/views.py
+++ order/views.py
@@ -6,6 +6,8 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 
 
+
+@csrf_exempt
 def shop(request):
      if request.method == 'GET':
           shop = Shop.objects.all()
@@ -13,5 +15,10 @@ def shop(request):
           serializer = ShopSerializer(shop, many=True)
           return JsonResponse(serializer.data, safe=False)
 
-     # if request.method == 'POST':
-     
+     elif request.method == 'POST':
+          data = JSONParser().parse(request)
+          serializer = ShopSerializer(data=data)
+          if serializer.is_valid():
+               serializer.save()
+               return JsonResponse(serializer.data, status=201)
+          return JsonResponse(serializer.errors, status=400)
