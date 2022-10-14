20e2644e7c7aab384969b318c0dab184681b3dbe
lhjwork <jin.come.up.business@gmail.com>
2022-10-14 14:41:32 +0900

    ch02_13 boss 백엔드 개발 1

---
+++ boss/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from boss import views
+
+urlpatterns = [
+     path('timeinput/', views.time_input, name = "timeinput"),
+     # path('api-auth/',include('rest_framework.urls', namespace='rest_framework')),
+     # path('menus/<int:shop>', views.menu, name = "menu"),
+     # path('order/', views.order, name = "order"),
+     
+]
