f4a7914b4aa3046840cb38d850aee4adbd39325e
lhjwork <jin.come.up.business@gmail.com>
2022-10-17 10:35:08 +0900

    urls.py 의 path 설정 잘못됨 timeinput/<int:shop> 설정에서 timeinput/으로 대치

--- boss/urls.py
+++ boss/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from boss import views
 
 urlpatterns = [
+     path('orders/<int:shop>',views.order_list, name = 'order_list'),
      path('timeinput/', views.time_input, name = "timeinput"),
      # path('api-auth/',include('rest_framework.urls', namespace='rest_framework')),
      # path('menus/<int:shop>', views.menu, name = "menu"),
